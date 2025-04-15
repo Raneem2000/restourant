540946f856d1b8320662315d4ff0f2b5c4c026f2
raneemonehand@gmail.com <Raneem@OneHand2024>
2025-04-15 21:18:15 +0300

    add to cart

--- js/script.js
+++ js/script.js
@@ -214,3 +214,21 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   }
 });
+
+// sidebar cart content
+function toggleSidebar() {
+  const sidebar = document.getElementById("sidebar");
+  const overlay = document.getElementById("overlay");
+
+  const isOpen = sidebar.classList.contains("translate-x-0");
+
+  if (isOpen) {
+    sidebar.classList.add("translate-x-full");
+    sidebar.classList.remove("translate-x-0");
+    overlay.classList.add("hidden");
+  } else {
+    sidebar.classList.remove("translate-x-full");
+    sidebar.classList.add("translate-x-0");
+    overlay.classList.remove("hidden");
+  }
+}
